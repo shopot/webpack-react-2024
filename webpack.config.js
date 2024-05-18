c1ecfa8627e6a19721d976b993a49d99cd0c6687
Sapronov Konstantin <ks.costas@gmail.com>
2024-05-19 00:41:24 +0300

    chore: fix css modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { default: merge } = require('webpack-merge');
 
 const createAliases = require('./__webpack__/webpack.alias');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const stylesHandler = isDev ? 'style-loader' : MiniCssExtractPlugin.loader;
 
 const rootDirectory = path.join(__dirname);
 const srcDirectory = path.join(rootDirectory, 'src');
 const publicDirectory = path.join(rootDirectory, 'public');
 const templateHtmlFile = path.join(srcDirectory, 'index.html'); // path to index.html
-
-const isDev = process.env.NODE_ENV === 'development';
 
 const config = {
   entry: path.join(srcDirectory, 'index.tsx'),
@@ -53,73 +56,65 @@ const config = {
         exclude: /(node_modules)/,
         loader: 'swc-loader',
         options: {
-          module: {
-            type: 'es6',
-          },
+          module: { type: 'es6' },
           minify: !isDev,
           isModule: true,
           jsc: {
-            minify: {
-              compress: true,
-              mangle: true,
-              format: {
-                asciiOnly: true,
-                comments: /^ webpack/,
-              },
-            },
+            minify: { compress: true, mangle: true, format: { asciiOnly: true, comments: /^ webpack/ } },
             target: 'esnext',
-            parser: {
-              syntax: 'typescript',
-              tsx: true,
-            },
-            transform: {
-              react: {
-                runtime: 'automatic',
-              },
-            },
+            parser: { syntax: 'typescript', tsx: true },
+            transform: { react: { runtime: 'automatic' } },
           },
         },
       },
       {
-        // For pure CSS - /\.css$/i,
-        // For Sass/SCSS - /\.((c|sa|sc)ss)$/i,
-        // For Less - /\.((c|le)ss)$/i,
         test: /\.((c|sa|sc)ss)$/i,
         use: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
+          stylesHandler,
           {
-            loader: 'postcss-loader',
+            loader: 'css-loader',
             options: {
-              postcssOptions: {
-                plugins: [['postcss-preset-env']],
+              importLoaders: 1,
+              esModule: false,
+              modules: {
+                localIdentName: isDev ? '[name]-[local]-[hash:base64:8]' : '[hash:base64]',
               },
             },
           },
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: [['postcss-preset-env']] } },
+          },
           'sass-loader',
         ],
+        include: /\.module\.(css|scss)$/,
+      },
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          stylesHandler,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: [['postcss-preset-env']] } },
+          },
+          'sass-loader',
+        ],
+        exclude: /\.module\.(css|scss)$/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|webp|gif)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'images/[hash][ext][query]',
-        },
+        generator: { filename: 'images/[hash][ext][query]' },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[hash][ext][query]',
-        },
+        generator: { filename: 'fonts/[hash][ext][query]' },
       },
     ],
   },
-  node: {
-    global: true,
-    __filename: true,
-    __dirname: true,
-  },
+  node: { global: true, __filename: true, __dirname: true },
 };
 
 module.exports = merge(
