d40605099654063f1c6892de735dd50f54b8ec7a
Sapronov Konstantin <ks.costas@gmail.com>
2024-05-19 00:47:00 +0300

    chore: update build docker image script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -17,3 +17,8 @@ docker build -t $PROJECT_NAME:"${IMAGE_SUFFIX}" -f ./Dockerfile .
 
 # Working only with Linux
 # docker save "webpack-react-2024":20240401_001 | gzip > webpack-react-2024_20240401_001.tar.gz
+
+echo "docker image ${PROJECT_NAME}:${IMAGE_SUFFIX} created."
+echo "docker save ${PROJECT_NAME}:${IMAGE_SUFFIX} | gzip > ${PROJECT_NAME}_${IMAGE_SUFFIX}.tar.gz"
+
+echo "Done!!!"
